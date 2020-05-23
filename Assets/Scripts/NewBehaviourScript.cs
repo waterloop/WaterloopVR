cce374334696a017e028575400590249fa33cd00
Johnny Chen <jyhchen03@gmail.com>
2020-05-23 14:05:35 -0600

    add collision example

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Sphere")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
