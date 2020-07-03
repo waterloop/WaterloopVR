da5c4129549b8f93fb8aa49b0f568b03107bbd2d
kamran14 <kam@kamrant.com>
2020-07-03 00:24:00 -0400

    cross-section of whole pod

---
+++ Assets/Scripts/CuttingScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+//[ExecuteInEditMode]
+public class CuttingScript : MonoBehaviour{
+
+    public GameObject plane;
+    public Material mat;
+    public Vector3 normal;
+    public Vector3 position;
+    public Renderer rend;
+    Renderer[] pod;
+    double i = 10;
+    // Use this for initialization
+    void Start() {
+        Debug.Log("Starting fun script");
+        rend = GetComponent<Renderer>();
+        normal = plane.transform.TransformVector(new Vector3(0, 0, -5));
+        position = plane.transform.position;
+        pod = GameObject.FindGameObjectWithTag("uniqueTag").GetComponentsInChildren<Renderer>();
+        foreach (Renderer x in pod) {
+            x.material = mat;
+        }
+        UpdateShaderProperties();
+    }
+    void Update() {
+
+        UpdateShaderProperties();
+    }
+
+    private void UpdateShaderProperties() {
+        // normal = plane.transform.TransformVector(new Vector3(0, 0, -1));
+        // position = plane.transform.position;
+        normal = new Vector3(0, 0, -1);
+        position = new Vector3(0, 0, (float)i);
+
+
+        if (Input.GetKey("up")) {
+            Debug.Log("i: " + i);
+            i += 0.05;
+        } else if (Input.GetKey("down")) {
+            Debug.Log("i: " + i);
+            i -= 0.05;
+        }
+        foreach (Renderer r in pod) {
+            r.material.SetVector("_PlanePosition", position);
+            r.material.SetVector("_PlaneNormal", normal);
+        }
+    }
+}
