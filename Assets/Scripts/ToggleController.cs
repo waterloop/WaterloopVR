3e42ab562ddbe9ebff3698f1546c4a1363094913
KaiYuanC <chikaiyuan@gmail.com>
2020-07-12 23:35:20 -0400

    add toggles for grouping

---
+++ Assets/Scripts/ToggleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleController : MonoBehaviour
+{
+    public bool toggleIsOn;
+    List<Toggle> groupToggles;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        groupToggles = new List<Toggle>();
+        foreach(GameObject gameObject in GameObject.FindGameObjectsWithTag("GroupToggle")){
+            groupToggles.Add(gameObject.GetComponent<Toggle>());
+        }
+        GetComponent<Button>().onClick.AddListener(onClick);
+    }
+
+    public void onClick() {
+        foreach(Toggle toggle in groupToggles) {
+            toggle.isOn = toggleIsOn;
+        }
+        
+    }
+}
+
