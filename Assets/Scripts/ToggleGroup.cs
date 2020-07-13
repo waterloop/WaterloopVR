47284b1b8e60e6c1a023a9067cb19e82c855ff4e
tamim-khan <tamim.m.khan@gmail.com>
2020-07-12 23:48:19 -0400

    cleanup

--- Assets/Scripts/ToggleGroup.cs
+++ Assets/Scripts/ToggleGroup.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ToggleGroup : MonoBehaviour
-{
+public class ToggleGroup : MonoBehaviour {
     Toggle toggle;
     GameObject[] gameObjects;
     public string tagName;
 
-    void Start()
-    {
+    void Start() {
         //Fetch the Toggle GameObject
         toggle = GetComponent<Toggle>();
-        //tagName = toggle.text; 
         gameObjects = GameObject.FindGameObjectsWithTag(tagName);
         //Add listener for when the state of the Toggle changes, to take action
         toggle.onValueChanged.AddListener(delegate {
@@ -22,8 +19,7 @@ public class ToggleGroup : MonoBehaviour
     }
 
     //Output the new state of the Toggle into Text
-    void ToggleValueChanged(Toggle toggle)
-    {
+    void ToggleValueChanged(Toggle toggle) {
         foreach (GameObject gameObject in gameObjects) {
             gameObject.SetActive(toggle.isOn);
         }
